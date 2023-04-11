31255d75bcccd2aabff8fb865a9b2c3addf57dd0
darraghmasterson <dmasters@tcd.ie>
2023-04-11 16:14:13 +0100

    fixed code

--- Script1.js
+++ Script1.js
@@ -33,31 +33,42 @@
                     var url = "https://api.spotify.com/v1/playlists/3AGMOTjNxCTqFOE48JDZUz/tracks";
                     var offset = 0;
                     var limit = 50;
-                    
+                    var allResults = []; // Array to store all results
+
                     var xhr = new XMLHttpRequest();
-                    
+
                     // Function to make API request
                     function makeRequest() {
-                      xhr.open("GET", url + `?offset=${offset}&limit=${limit}`);
-                      xhr.setRequestHeader("Authorization", `Bearer ${access_token}`);
-                      xhr.onreadystatechange = function () {
+                    xhr.open("GET", url + `?offset=${offset}&limit=${limit}`);
+                    xhr.setRequestHeader("Authorization", `Bearer ${access_token}`);
+                    xhr.onreadystatechange = function () {
                         if (xhr.readyState === 4) {
-                          console.log(xhr.status);
-                          var json = JSON.parse(xhr.responseText);
-                          console.log(json);
-                    
-                          // Check if there are more results
-                          if (json.next) {
+                        if (xhr.status === 200) {
+                            var json = JSON.parse(xhr.responseText);
+                            console.log(json);
+
+                            // Concatenate results to the allResults array
+                            allResults = allResults.concat(json.items);
+
+                            // Check if there are more results
+                            if (json.next) {
                             // If there are more results, update the offset and make another request
                             offset += limit;
                             makeRequest();
-                          }
+                            } else {
+                            console.log(allResults); // All results stored in one array
+                            }
+                        } else {
+                            console.error("Failed to fetch data from Spotify API: ", xhr.status);
                         }
-                      };
-                      xhr.send();
+                        }
+                    };
+                    xhr.send();
                     }
-                    
+
+                    // Start the initial API request
                     makeRequest();
+
                     
 
                 };
